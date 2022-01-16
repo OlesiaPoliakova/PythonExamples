b20930ebe45e489729383238891f29bd4ddbc96e
Olesia Poliakova <lesyapolyk@gmail.com>
2022-01-16 20:56:43 +0200

    HomeWork9TimeMachine

--- Time Machine.py
+++ Time Machine.py
@@ -1,18 +1,8 @@
-from datetime import date
 
-date(year_int, month_int, day_int)
-first_year = 1832 // 100 * 100
-last_year = first_year + 100
-year_given_by_time_machine = random.randint(first_year, last_year)
-• Year – date_obj.year
-• Month – date_obj.month
-• Day – date_obj.day
-# Generator comprehention
-data1 = (int(item) for item in data_list1)
+def countdown(given_sec):
+    counter = (seconds for seconds in range(given_sec, -1, -1))
+    return counter
 
-# while True:
-#     pass
-# If you get the time_machine  result
-# # with the desired year, just break the loop and print the WINNER MESSAGE 
-# 30 вариантов дат, но напечатайте
-# yield только те даты, которые относятся к желаемому десятилетию.
+for remaining_seconds in countdown(20):
+    print(remaining_seconds)
+
