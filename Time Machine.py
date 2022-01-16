9c569e8561b9afe6346e7436d544f3def51ec524
Olesia Poliakova <lesyapolyk@gmail.com>
2022-01-16 21:27:10 +0200

    HomeWork9TimeMachine

--- Time Machine.py
+++ Time Machine.py
@@ -1,8 +1,9 @@
-
+import time
 def countdown(given_sec):
     counter = (seconds for seconds in range(given_sec, -1, -1))
     return counter
 
 for remaining_seconds in countdown(20):
     print(remaining_seconds)
+    time.sleep(1)
 
